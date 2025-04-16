9599adf78f2715e06a9e7d8b12efe831985cbe9d
Mathew Ritchie <ritchmat89@gmail.com>
2025-04-16 10:41:41 +0200

    modal functionality
    added toggle and closing functionality

--- my-react-app/src/components/MainSortingHeader.jsx
+++ my-react-app/src/components/MainSortingHeader.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import GenreDropDown from "./SortingAndFiltering/GenreDropDown";
 import SearchInput from "./SortingAndFiltering/SearchInput";
 import SortingDropDown from "./SortingAndFiltering/SortingDropDown";
 import "./main-sorting-headers.css";
 import { HiAdjustmentsHorizontal } from "react-icons/hi2";
-import SortingModal from "./SortingAndFiltering/SortingModal";
 
 export default function SortingHeader() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalVisible(!isModalVisible);
+  };
+
+  const hideModal = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <nav className="show-nav">
-      <div className="sorting-input-wrapper">
+      <div className="sorting-input-wrapper" style={{ display: isModalVisible ? "flex" : "none" }}>
         <SearchInput />
         <GenreDropDown />
         <SortingDropDown />
-        <button className="modal-close-button">Close</button>
+        <button className="modal-close-button" onClick={hideModal}>
+          Close
+        </button>
       </div>
-      <button class="sorting-button" id="hamburger">
+      <button className="open-sorting-button" id="hamburger" onClick={toggleModal}>
         <HiAdjustmentsHorizontal />
       </button>
     </nav>
